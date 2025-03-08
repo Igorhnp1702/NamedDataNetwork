9cace2d47c592a65a12309a833158c2d4fab9e6b
MonicaFerreira5 <monicaamferreira5@gmail.com>
2025-03-08 16:07:25 +0000

    Apaguei ndn_func .c .h e passei para o ndn_io.h aquilo que interessava

--- ndn_io.h
+++ ndn_io.h
@@ -59,6 +59,111 @@
 #define new_str "NEW"             // TCP msg NEW
 #define extern_str "EXTERN"       // TCP msg EXTERN
 
+
+/**************************************************************************
+ * nodeinfo_t
+ *
+ *  Description:
+ *
+ *  -Data type in the form of a struct that holds the contact of a node;
+ *
+ *  Members of the struct:
+ *
+ *  -network = index of the network; integer from 000 to 999;
+ *  -node_id = tcp port of the node; integer from 0 to 65 536;
+ *  -node_addr = index of the node; integer from 00 to 99;
+ *  -node_port = IPv4 address with undefined size;
+ ***************************************************************************/
+ typedef struct any_node
+ {
+    char *network;   // index of the network; integer from 000 to 999
+    char *tcp_port;  // tcp port of the node; integer from 0 to 65 536
+    char *node_id;   // index of the node; integer from 00 to 99
+    char *node_addr; // IPv4 address with undefined size
+ } nodeinfo_t;
+ 
+ /**************************************************************************
+  * struct Linkedlist
+  * 
+  * Description: 
+  * 
+  *  -Structure of variables to build linked lists of strings
+  * 
+  * Members of the struct
+  * 
+  *  -string = dynamic array of chars; 
+  *  -next = pointer to the next string; 
+  **************************************************************************/
+ struct Linkedlist{
+    char *string;            // dynamic array of chars
+    struct Linkedlist *next; // pointer to the next name
+ };
+ 
+ /**************************************************************************
+  * contentList_t
+  * 
+  * Description: Data type for the list of contents;
+  ***************************************************************************/
+ typedef struct Linkedlist contentList_t;
+ 
+ /***************************************************************************
+  * queryList_t
+  * 
+  * Description: Data type for the list of queries sent;
+  ***************************************************************************/
+ typedef struct Linkedlist queryList_t;
+ 
+ /*****************************************************************************
+  * personal_node
+  *
+  *  Description:
+  *
+  *  -Structure of data that will hold all variables regarding the personal node;
+  *
+  *  Members of the struct:
+  *
+  *  -persn_info  = contact of the personal node;
+  *  -anchorflag  = flag that indicates if the node is an anchor or not
+  *  -n_neighbrs  = counter for the number of internal neighbors;
+  *  -udp_fd      = file descriptor to communicate with the node server
+  *  -server_fd   = file descriptor to communicate with the intern nodes
+  *  -max_fd      = the maximum integer assigned to a file descriptor in this node's FD set
+  *  -client_fd   = file descriptor to communicate with the extern node
+  *  -neighbrs_fd = array that relates internal neighbrs id's with their fd. The indexes match the nodes's ids
+  *  -route_tab   = expedition table. The indexes of the array are the destinations
+  *  -udp_port    = UDP port of the server of nodes
+  *  -udp_address = UDP address of the server of nodes
+  *  -rdy_scks    = set of file descriptors to read from
+  *  -crr_scks    = set of file descriptors to read from
+  *  -contents    = linked list of the contents of the node
+  *  -extern_node = contact of the extern neighbor node
+  *  -backup_node = contact of the backup neighbor node
+  *  -neighbrs    = array of contacts of intern neighbors. 
+  *                   The indexes match the nodes's ids
+  ****************************************************************************/
+ struct personal_node
+ {
+    nodeinfo_t *persn_info;    // contact of the personal node
+    int anchorflag;            // flag that says whether the node is an anchor or not
+    int n_neighbrs;            // counter for the number of internal neighbors
+    int udp_fd;                // file descriptor to communicate with the server of nodes//!não esta a ser utilizado
+    int server_fd;             // file descriptor to communicate with the intern nodes
+    int max_fd;                // the maximum integer assigned to a file descriptor in this node's FD set
+    int client_fd;             // file descriptor to communicate with the extern node
+    int neighbrs_fd[NODE_IDS]; // array that relates internal neighbrs id's with their fd. The indexes match the nodes's ids
+    int *route_tab;            // expedition table. The indexes of the array are the destinations
+    char *udp_port;            // UDP port of the server of nodes
+    char *udp_address;         // UDP address of the server of nodes
+    fd_set rdy_scks;           // set of file descriptors with activity to handle
+    fd_set crr_scks;           // set of file descriptors in use
+    queryList_t *qryhistory;   // history of queries sent
+    contentList_t *contents;   // linked list of the contents of the node
+    nodeinfo_t *extern_node;   // contact of the extern neighbor node
+    nodeinfo_t *backup_node;   // contact of the backup neighbor node
+    nodeinfo_t **neighbrs;     // array of contacts of intern neighbors. The indexes match the nodes's ids
+ };
+
+
 int is_valid_ip(const char *ip);
 
 int check_num_string(char *num_string);
